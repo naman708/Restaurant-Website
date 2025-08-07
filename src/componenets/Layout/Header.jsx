62d2c924e97b45fa3fe4b8733e69e913498bb649
naman708 <pawarnaman11@gmail.com>
2025-08-07 16:13:55 +0530

    feat : create header for app

---
+++ src/componenets/Layout/Header.jsx
@@ -0,0 +1,20 @@
+
+import classes from "./Header.module.css"
+
+import HeaderCartButton from "./HeaderCartButton";
+
+function Header(){
+ return(
+ <>
+ <header className={classes.header}>
+    <h1>React Meals</h1>
+    <HeaderCartButton/>
+ </header>
+ <div className={classes['main-image']}>
+  <img src="https://github.com/academind/react-complete-guide-code/blob/11-practice-food-order-app/extra-files/meals.jpg?raw=true" />
+ </div>
+ </>
+ )
+
+}
+export default Header;
